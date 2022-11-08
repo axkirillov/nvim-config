2cf5992b11a9136fbb7226838fa64feaab84ce29
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2022-11-08 11:52:19 +0100

    add run make target picker

--- plugin/easypick.lua
+++ plugin/easypick.lua
@@ -8,8 +8,27 @@ local list = [[
 :Easypick changed_files
 :Easypick conflicts
 :Easypick config_files
+:Easypick make_targets
 EOF
 ]]
+
+local list_make_targets = [[
+make -qp |
+awk -F':' '/^[a-zA-Z0-9][^$#\/\t=]*:([^=]|$)/ {split($1,A,/ /);for(i in A)print A[i]}' |
+grep -wv Makefile
+]]
+
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local function run_make_target(prompt_bufnr, _)
+  actions.select_default:replace(function()
+    actions.close(prompt_bufnr)
+    local selection = action_state.get_selected_entry()
+    vim.cmd("FloatermNew make " .. selection[1])
+  end)
+  return true
+end
 
 easypick.setup({
 	pickers = {
@@ -33,9 +52,16 @@ easypick.setup({
 			command = "cat " .. list,
 			action = easypick.actions.run_nvim_command,
 			opts = require('telescope.themes').get_dropdown({})
+		},
+		{
+			name = "make_targets",
+			command = list_make_targets,
+			action = run_make_target,
+			opts = require('telescope.themes').get_dropdown({})
 		}
 	}
 })
 
 local ns = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-p>", ":Easypick command_palette<CR>", ns)
+vim.keymap.set("n", "<leader>m", ":Easypick make_targets<CR>", ns)
