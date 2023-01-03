939ced2ac7ef136009429961edca2a55870f00da
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-01-03 11:52:27 +0100

    disable git integration for performance

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -7,26 +7,30 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	git = {
+		enable = false,
+		ignore = true,
+		show_on_dirs = true,
+		show_on_open_dirs = true,
+		timeout = 400,
+	},
 })
 
 vim.keymap.set('n', '<C-n>', '<Cmd>NvimTreeFindFileToggle<CR>')
