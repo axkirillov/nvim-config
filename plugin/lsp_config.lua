4654448e4726ca25007ff825596d3b11d40d9746
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-02-17 16:17:34 +0100

    format lsp congig

--- plugin/lsp_config.lua
+++ plugin/lsp_config.lua
@@ -1,6 +1,6 @@
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -10,44 +10,45 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- after the language server attaches to the current buffer
 ---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local telescope_builtin = require('telescope.builtin');
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  --vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations, bufopts)
-  vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  --vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  --vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  --vim.keymap.set('n', '<space>wl', function()
-  --  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  --end, bufopts)
-  --vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, bufopts)
-  --vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
+	local telescope_builtin = require('telescope.builtin');
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	--vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations, bufopts)
+	vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	--vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	--vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	--vim.keymap.set('n', '<space>wl', function()
+	--  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	--end, bufopts)
+	--vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, bufopts)
+	--vim.keymap.set('n', '<space>fm', vim.lsp.buf.format, bufopts)
+	vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require'lspconfig'.sumneko_lua.setup {
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
+require 'lspconfig'.sumneko_lua.setup {
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
 }
 
-require'lspconfig'.intelephense.setup {
+require 'lspconfig'.intelephense.setup {
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = {
@@ -68,10 +69,14 @@ require'lspconfig'.intelephense.setup {
 	}
 }
 
-require'lspconfig'.gopls.setup {
-    on_attach = on_attach,
+require 'lspconfig'.gopls.setup {
+	on_attach = on_attach,
 }
 
-require'lspconfig'.tsserver.setup{
-    on_attach = on_attach,
+require 'lspconfig'.tsserver.setup {
+	on_attach = on_attach,
+}
+
+require 'lspconfig'.terraformls.setup {
+	on_attach = on_attach
 }
