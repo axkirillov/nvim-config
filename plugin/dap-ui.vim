28560f880ba6f792dfda6208ad9cc88fc36f0b4d
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-02-24 12:22:28 +0100

    configure dap ui (its garbage tho)

---
+++ plugin/dap-ui.vim
@@ -0,0 +1,1 @@
+noremap <M-k> <Cmd>lua require("dapui").eval()<CR>
--- plugin/dap_ui.lua
+++ plugin/dap_ui.lua
@@ -1,68 +1,95 @@
-require("dapui").setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = vim.fn.has("nvim-0.7"),
-  -- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position. It can be an Int
-  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-  -- Elements are the elements shown in the layout (in order).
-  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-  layouts = {
-    {
-      elements = {
-      -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        "breakpoints",
-        "stacks",
-        "watches",
-      },
-      size = 40, -- 40 columns
-      position = "left",
-    },
-    {
-      elements = {
-        "repl",
-        "console",
-      },
-      size = 0.25, -- 25% of total lines
-      position = "bottom",
-    },
-  },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "single", -- Border style. Can be "single", "double" or "rounded"
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
-  windows = { indent = 1 },
-  render = {
-    max_type_length = nil, -- Can be integer or nil.
-  }
-})
-
 local dap, dapui = require("dap"), require("dapui")
+
+dapui.setup(
+	{
+		controls = {
+			element = "repl",
+			enabled = true,
+			icons = {
+				disconnect = "",
+				pause = "",
+				play = "",
+				run_last = "",
+				step_back = "",
+				step_into = "",
+				step_out = "",
+				step_over = "",
+				terminate = ""
+			}
+		},
+		element_mappings = {},
+		expand_lines = true,
+		floating = {
+			border = "single",
+			mappings = {
+				close = { "q", "<Esc>" }
+			}
+		},
+		force_buffers = true,
+		icons = {
+			collapsed = "",
+			current_frame = "",
+			expanded = ""
+		},
+		layouts = {
+			--{
+			--	elements = {
+			--		{
+			--			id = "scopes",
+			--			size = 0.25,
+			--		},
+			--		{
+			--			id = "breakpoints",
+			--			size = 0.25
+			--		},
+			--		{
+			--			id = "stacks",
+			--			size = 0.25
+			--		},
+			--		{
+			--			id = "watches",
+			--			size = 0.25
+			--		}
+			--	},
+			--	position = "left",
+			--	size = 40
+			--},
+			{
+				elements = {
+					{
+						id = "repl",
+						size = 0.5
+					},
+					--{
+					--	id = "console",
+					--	size = 0.5
+					--}
+				},
+				position = "bottom",
+				size = 10
+			}
+		},
+		mappings = {
+			edit = "e",
+			expand = { "<CR>", "<2-LeftMouse>" },
+			open = "o",
+			remove = "d",
+			repl = "r",
+			toggle = "t"
+		},
+		render = {
+			indent = 1,
+			max_value_lines = 100
+		}
+	}
+)
+
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
-
--- vnoremap <M-k> <Cmd>lua require("dapui").eval()<CR>
