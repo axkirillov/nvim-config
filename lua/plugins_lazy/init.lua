1f8697eaf4380fa3ff204cc8991a9104cff93571
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-05-05 11:43:04 +0200

    rm advanced git search

--- lua/plugins_lazy/init.lua
+++ lua/plugins_lazy/init.lua
@@ -84,18 +84,6 @@ return {
 
 	'echasnovski/mini.bracketed',
 
---	{
---		"aaronhallaert/ts-advanced-git-search.nvim",
---		config = function()
---			require("telescope").load_extension("advanced_git_search")
---		end,
---		dependencies = {
---			"nvim-telescope/telescope.nvim",
---			-- to show diff splits and open commits in browser
---			"tpope/vim-fugitive",
---		},
---	},
-
 	'towolf/vim-helm',
 
 	'github/copilot.vim',
