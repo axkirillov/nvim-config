5a3de17332d0a8661428b5dda550d8ea3bc0a7a4
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2023-09-21 12:47:06 +0200

    mv telecope config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,2 @@
 require("plugins.lsp_config")
-require("plugins.telescope")
 require("plugins.dap_ui")
--- lua/plugins_lazy/telescope.lua
+++ lua/plugins_lazy/telescope.lua
@@ -1,10 +1,57 @@
+local setup = function()
+	local actions = require("telescope.actions")
+	require "telescope".setup {
+		defaults = {
+			mappings = {
+				n = {
+				},
+				i = {
+					["<esc>"] = actions.close,
+					["<C-j>"] = actions.select_default,
+				}
+
+			},
+			preview = {
+				treesitter = false
+			},
+			layout_config = {
+				height = 0.95,
+				width = 0.99
+			},
+			wrap_results = true
+		},
+
+		pickers = {
+			find_files = {
+				find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "-E", ".git", "-H" },
+			},
+			git_branches = {
+				theme = "dropdown",
+				previewer = false
+			}
+		},
+	}
+
+	require('telescope').load_extension('fzf')
+	require "telescope".load_extension("agrolens")
+
+	local ns = { noremap = true, silent = true }
+	vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", ns)
+	vim.keymap.set("n", "<leader>b", ":Hbac telescope<CR>", ns)
+	vim.keymap.set("n", "<leader>t", ":Telescope<CR>", ns)
+	vim.keymap.set("n", "<leader>lg", ":Telescope live_grep<CR>", ns)
+	vim.keymap.set("n", "<leader>g", ":Telescope git_status<CR>", ns)
+	local symbols = function()
+		require 'telescope.builtin'.lsp_document_symbols(require('telescope.themes').get_dropdown({ previewer = false }))
+	end
+	vim.keymap.set("n", "<leader>s", symbols, ns)
+end
+
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	dependencies = {
 		{ 'nvim-lua/plenary.nvim' },
 	},
-	config = function()
-		require("plugins.telescope")
-	end,
+	config = setup,
 }
 
