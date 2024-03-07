009f9708b8954c365982d33e89d4bae3aa467d53
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-03-07 11:49:09 +0100

    c-j to select in cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -25,7 +25,7 @@ local setup = function()
 			['<C-f>'] = cmp.mapping.scroll_docs(4),
 			['<C-Space>'] = cmp.mapping.complete(),
 			['<C-e>'] = cmp.mapping.abort(),
-			['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			['<C-j>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			-- ["<Tab>"] = cmp.mapping(function(fallback)
 			-- 	if cmp.visible() then
 			-- 		cmp.select_next_item()
