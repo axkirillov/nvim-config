01cec226816d63d05e5be79ef992530c1ed6d54b
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-03-07 11:49:26 +0100

    update outline config

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,12 +1,21 @@
 return {
-  "hedyhli/outline.nvim",
-  config = function()
-    -- Example mapping to toggle outline
-    vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
-      { desc = "Toggle Outline" })
+	"hedyhli/outline.nvim",
+	config = function()
+		-- Example mapping to toggle outline
+		vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+			{ desc = "Toggle Outline" })
 
-    require("outline").setup {
-      -- Your setup opts here (leave empty to use defaults)
-    }
-  end,
+		require("outline").setup {
+			outline_window = {
+				auto_jump = true,
+				width = 15,
+			},
+			outline_items = {
+				show_symbol_details = false,
+			},
+			keymaps = {
+				goto_location = '<C-j>',
+			},
+		}
+	end,
 }
