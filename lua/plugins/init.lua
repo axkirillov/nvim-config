b6a0c3eab49f945286573ba2fbbc5bb443051caa
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-04-15 16:47:10 +0200

    update hbac telescope mappings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -85,8 +85,39 @@ return {
 
 	{
 		'axkirillov/hbac.nvim',
+		dependencies = {
+			'nvim-telescope/telescope.nvim',
+		},
 		config = function()
-			require("hbac").setup()
+			-- These actions refresh the picker and the pin states/icons of the open buffers
+			-- Use these instead of e.g. `hbac.pin_all()`
+			local actions = require("hbac.telescope.actions")
+			require("hbac").setup(
+				{
+					telescope = {
+						sort_mru = true,
+						sort_lastused = true,
+						selection_strategy = "row",
+						mappings = {
+							i = {
+								-- ["<M-c>"] = actions.close_unpinned,
+								["<C-x>"] = actions.delete_buffer,
+								-- ["<M-a>"] = actions.pin_all,
+								-- ["<M-u>"] = actions.unpin_all,
+								-- ["<M-y>"] = actions.toggle_pin,
+							},
+							n = {
+								-- as above
+							},
+						},
+						-- Pinned/unpinned icons and their hl groups. Defaults to nerdfont icons
+						pin_icons = {
+							pinned = { "󰐃 ", hl = "DiagnosticOk" },
+							unpinned = { "󰤱 ", hl = "DiagnosticError" },
+						},
+					}
+				}
+			)
 		end,
 		branch = "develop"
 	},
