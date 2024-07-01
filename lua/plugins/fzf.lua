db0d8122cb732f82f9091e39447d5b3bcc4a20ca
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-07-01 16:51:43 +0200

    put some plugins in their own file

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,7 +3,6 @@ vim.keymap.set("n", "<leader>rg", ":FzfLua grep_project<CR>", { noremap = true, 
 vim.keymap.set("n", "<leader>b", ":FzfLua buffers<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>g", ":FzfLua git_status<CR>", { noremap = true, silent = true })
 
-
 local branch_diff = function()
 	local default_branch = vim.fn.system("git remote show origin | grep 'HEAD branch' | cut -d' ' -f5") or
 	"main"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,7 @@
 return {
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
-
-	{ 'dag/vim-fish' },
-
+	'dag/vim-fish',
 	'tpope/vim-fugitive',
 
 	-- completion
@@ -11,50 +9,13 @@ return {
 	'hrsh7th/cmp-buffer',
 	'hrsh7th/cmp-path',
 	'hrsh7th/cmp-cmdline',
-
 	'saadparwaiz1/cmp_luasnip',
 
 	'samoshkin/vim-mergetool',
-
-	-- Diffview
-	{ 'sindrets/diffview.nvim', dependencies = 'nvim-lua/plenary.nvim' },
-
-	-- Justfile syntax highlighting
 	'NoahTheDuke/vim-just',
-
 	"folke/neodev.nvim",
-
-	{
-		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-		config = function()
-			vim.diagnostic.config({
-				virtual_text = false,
-			})
-
-			require("lsp_lines").setup()
-		end,
-	},
-
 	'towolf/vim-helm',
-
 	'github/copilot.vim',
-
-	{
-		"folke/noice.nvim",
-		config = function()
-			require("noice").setup({
-				-- add any options here
-			})
-		end,
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		}
-	},
 
 	-- open git files in browser
 	'tpope/vim-rhubarb',
