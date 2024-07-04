c3c10082f4a3f5b49d6c94528acc1d97f4a4dd41
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2024-07-04 10:35:48 +0200

    update fzflua commands

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,9 +3,12 @@ vim.keymap.set("n", "<leader>rg", ":FzfLua grep_project<CR>", { noremap = true, 
 vim.keymap.set("n", "<leader>b", ":FzfLua buffers<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>g", ":FzfLua git_status<CR>", { noremap = true, silent = true })
 
+local default_branch
 local branch_diff = function()
-	local default_branch = vim.fn.system("git remote show origin | grep 'HEAD branch' | cut -d' ' -f5") or
-	"main"
+	if not default_branch then
+		local command = "git remote show origin | grep 'HEAD branch' | cut -d' ' -f5"
+		default_branch = vim.fn.system(command) or "main"
+	end
 	local command = "git diff --name-only $(git merge-base HEAD " .. default_branch .. " )"
 	require 'fzf-lua'.files({ cmd = command })
 end
@@ -15,8 +18,34 @@ local conflicts = function()
 	require 'fzf-lua'.files({ cmd = command })
 end
 
-vim.keymap.set("n", "<leader>pf", branch_diff, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>pc", conflicts, { noremap = true, silent = true })
+local function_table = {
+	["branch_diff"] = branch_diff,
+	["conflicts"] = conflicts,
+}
+
+local function run_lua_function_picker()
+	local fzf = require('fzf-lua')
+	local function_names = {}
+	for name, _ in pairs(function_table) do
+		table.insert(function_names, name)
+	end
+
+	fzf.fzf_exec(function_names, {
+		prompt = "Select a Lua function to run: ",
+		actions = {
+			["default"] = function(selected)
+				local func = function_table[selected[1]]
+				if func then
+					func()
+				else
+					print("Function not found")
+				end
+			end
+		}
+	})
+end
+
+vim.keymap.set("n", "<c-p>", run_lua_function_picker, { noremap = true, silent = true })
 
 return {
 	'ibhagwan/fzf-lua',
