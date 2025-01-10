8d98e33b05195969fa7a169e98a2c312a2e70112
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-10 13:35:06 +0100

    disable window navigation

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -60,3 +60,6 @@ vim.keymap.set('', '<F1>', ':!tmux popup -E -w 160 -h 40 -d $(pwd)<cr>')
 
 -- Git blame (requires fugitive)
 vim.keymap.set('n', 'gb', ':Gblame<CR>')
+
+-- disable c-w h
+vim.keymap.set('n', '<C-w>', '<Nop>')
