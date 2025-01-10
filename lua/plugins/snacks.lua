2166c5f1ed881f9c663e62a9c01d5e77c3179488
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-10 13:35:24 +0100

    update snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,15 +1,20 @@
 return
 {
-	"folke/snacks.nvim",
-	opts = {
-		lazygit = {
-			config = {
-				os = { editPreset = "nvr" }
-			}
-		}
-	},
-	config = function()
-		local snacks = require("snacks")
-		vim.keymap.set("n", "<C-g>", function() snacks.lazygit() end, { noremap = true, silent = true })
-	end
+  "folke/snacks.nvim",
+  opts = {
+    lazygit = {
+      config = {
+        os = {
+          editPreset = "nvim-remote",
+          --    this does not work, i dunno why
+          edit =
+          '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})'
+        }
+      }
+    }
+  },
+  config = function()
+    local snacks = require("snacks")
+    vim.keymap.set("n", "<C-g>", function() snacks.lazygit() end, { noremap = true, silent = true })
+  end
 }
