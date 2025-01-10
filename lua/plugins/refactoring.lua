931f7d10e964aa8aac4fd51caf7500900343f520
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-10 13:35:16 +0100

    install refactoring

---
+++ lua/plugins/refactoring.lua
@@ -0,0 +1,11 @@
+return {
+	"ThePrimeagen/refactoring.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	lazy = false,
+	config = function()
+		require("refactoring").setup()
+	end,
+}
