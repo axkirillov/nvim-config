56b5c1790596b15cc8e84c9747f248dfe0ed1360
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-22 16:39:35 +0100

    add aider command to snack terminal

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,6 +19,27 @@ return
 	config = function()
 		local snacks = require("snacks")
 		vim.keymap.set("n", "<C-g>", function() snacks.lazygit() end, { noremap = true, silent = true })
-		vim.keymap.set("n", "<C-t>", function() snacks.terminal.toggle() end, { noremap = true, silent = true })
+		vim.keymap.set(
+			{ "n", "t" },
+			"<C-t>",
+			function()
+				snacks.terminal.toggle()
+				-- reload current buffer if it was changed
+				vim.cmd("checktime")
+			end,
+			{ noremap = true, silent = true }
+		)
+		vim.keymap.set(
+			{ "n", },
+			"<leader>a",
+			function()
+				-- get current file path
+				local filepath = vim.api.nvim_buf_get_name(0)
+				snacks.terminal.toggle()
+				--paste file path
+				vim.api.nvim_feedkeys("/add " .. filepath, "n", false)
+			end,
+			{ noremap = true, silent = true }
+		)
 	end
 }
