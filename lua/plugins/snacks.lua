96fece84be5772f447fb68c39ff7effb7cad7fa8
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-22 10:05:01 +0100

    add snack term

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,10 +11,14 @@ return
 					'[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})'
 				}
 			}
-		}
+		},
+		terminal = {
+
+		},
 	},
 	config = function()
 		local snacks = require("snacks")
 		vim.keymap.set("n", "<C-g>", function() snacks.lazygit() end, { noremap = true, silent = true })
+		vim.keymap.set("n", "<C-t>", function() snacks.terminal.toggle() end, { noremap = true, silent = true })
 	end
 }
