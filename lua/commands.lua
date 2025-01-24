67aaba3c3b29b2b27bef00dccbba09d0c3b0a041
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-24 09:02:12 +0100

    rm unused commands

--- lua/commands.lua
+++ lua/commands.lua
@@ -10,42 +10,11 @@ end, {})
 -- Set syntax to gotmpl
 vim.api.nvim_create_user_command('GoTmpl', 'set filetype=gotmpl', {})
 
--- Add strict_types to PHP files
-vim.api.nvim_create_user_command('AddPHPStrictTypes', function()
-	local cursor_save = vim.fn.getpos('.')
-
-	-- Go to the beginning of the file
-	vim.fn.cursor(1, 1)
-
-	-- Find the line containing "<?php"
-	local phpLine = vim.fn.search('<?php')
-
-	-- Check if declaration isn't already present
-	if vim.fn.search('<?php\n\ndeclare(strict_types=1);') == 0 and phpLine ~= 0 then
-		-- Add the declaration after the line
-		vim.fn.append(phpLine, { "", "declare(strict_types=1);" })
-	end
-
-	-- Restore cursor position
-	vim.fn.setpos('.', cursor_save)
-end, {})
-
 -- Frequent typos
 vim.api.nvim_create_user_command('W', 'w', {})
 vim.api.nvim_create_user_command('Wq', 'wq', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('Qa', 'qa', {})
-
--- Close all buffers but the current one
-vim.api.nvim_create_user_command('CloseAllButThisOne', function()
-	vim.cmd('%bd')
-	vim.cmd('e#')
-end, {})
-
--- Toggle LSP lines
-vim.api.nvim_create_user_command('LspLines', function()
-	require("lsp_lines").toggle()
-end, {})
 
 -- Open line in GitHub
 vim.api.nvim_create_user_command('OpenLineInGithub', '.GBrowse', {})
