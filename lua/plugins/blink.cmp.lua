9a25bc863cc12938a3d09990d900a2e7dcc4ad56
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-28 08:59:02 +0100

    rm snippets

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,8 +1,5 @@
 return {
 	'saghen/blink.cmp',
-	-- optional: provides snippets for the snippet source
-	dependencies = 'rafamadriz/friendly-snippets',
-
 	-- use a release tag to download pre-built binaries
 	version = 'v0.8.2',
 	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
