a44eb89e9fbb2bd3eaf65fe45927d4c6894b8651
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-01-31 17:05:58 +0100

    rm snippets source

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -48,7 +48,7 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { 'lsp', 'path', 'buffer' },
 		},
 	},
 	opts_extend = { "sources.default" }
