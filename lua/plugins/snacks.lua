7c6c3412bd288e17f7147350fdb8bba3ff88faab
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-02-11 10:17:29 +0100

    rm lazygit config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,10 +5,6 @@ return
 		lazygit = {
 			config = {
 				os = {
-					editPreset = "nvim-remote",
-					--    this does not work, i dunno why
-					edit =
-					'[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})'
 				}
 			}
 		},
