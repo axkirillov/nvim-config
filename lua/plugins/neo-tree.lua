6e9d9b20a13779df95fd376e2855aacda3b346e2
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-02-13 09:08:40 +0100

    make neo tree smaller width

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -288,7 +288,7 @@ local config = {
 		-- see https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/popup for
 		-- possible options. These can also be functions that return these options.
 		position = "left",       -- left, right, top, bottom, float, current
-		width = 40,              -- applies to left and right positions
+		width = 30,              -- applies to left and right positions
 		height = 15,             -- applies to top and bottom positions
 		auto_expand_width = false, -- expand the window when file exceeds the window width. does not work with position = "float"
 		popup = {
