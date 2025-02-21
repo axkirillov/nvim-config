3e4dc55de32ee7430b0bc5b38fdea570cd3b8bde
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-02-21 11:59:19 +0100

    rm aider autoload

--- lua/plugins/aider.lua
+++ lua/plugins/aider.lua
@@ -5,16 +5,6 @@ return {
 	"GeorgesAlkhouri/nvim-aider",
 	lazy = false,
 	keys = {
-		--{ "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
-		--{ "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
-		--{ "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
-		--{ "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
-		--{ "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
-		--{ "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
-		--{ "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
-		---- Example nvim-tree.lua integration if needed
-		--{ "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
-		--{ "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
 	},
 	dependencies = {
 		"folke/snacks.nvim",
@@ -46,15 +36,6 @@ return {
 		)
 
 		aider.setup(config)
-
-		vim.api.nvim_create_autocmd("VimEnter", {
-			group = vim.api.nvim_create_augroup("Aider", { clear = true }),
-			callback = function()
-				terminal.toggle()
-				terminal.toggle()
-				vim.cmd("stopinsert")
-			end
-		})
 
 		local keymap_opts = {
 			noremap = true,
