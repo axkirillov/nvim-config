9c6f76f6a5b34baed714f5c31d64454cc1ed8b19
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-02-21 13:32:48 +0100

    fmt autocmd

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,39 +1,39 @@
 -- Search Highlighting Group
 local search_highlight_group = vim.api.nvim_create_augroup("SearchHighlighting", { clear = true })
 vim.api.nvim_create_autocmd("CmdlineEnter", {
-  pattern = { "/", "?" },
-  group = search_highlight_group,
-  callback = function()
-    vim.o.hlsearch = true
-  end
+	pattern = { "/", "?" },
+	group = search_highlight_group,
+	callback = function()
+		vim.o.hlsearch = true
+	end
 })
 vim.api.nvim_create_autocmd("CmdlineLeave", {
-  pattern = { "/", "?" },
-  group = search_highlight_group,
-  callback = function()
-    vim.o.hlsearch = false
-  end
+	pattern = { "/", "?" },
+	group = search_highlight_group,
+	callback = function()
+		vim.o.hlsearch = false
+	end
 })
 
 -- General Settings Group
 local general_group = vim.api.nvim_create_augroup("GeneralSettings", { clear = true })
 vim.api.nvim_create_autocmd("BufModifiedSet", {
-  group = general_group,
-  callback = function()
-    vim.cmd("silent! w")
-  end,
-  desc = "Auto Save"
+	group = general_group,
+	callback = function()
+		vim.cmd("silent! w")
+	end,
+	desc = "Auto Save"
 })
 
 -- Restore cursor to file position in previous editing session
 vim.api.nvim_create_autocmd("BufReadPost", {
-    callback = function(args)
-        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
-        local line_count = vim.api.nvim_buf_line_count(args.buf)
-        if mark[1] > 0 and mark[1] <= line_count then
-            vim.cmd('normal! g`"zz')
-        end
-    end,
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.cmd('normal! g`"zz')
+		end
+	end,
 })
 
 -- Add a VimEnter autocommand to create a persistent left padding window
