bd1f14a6334af76765bbc7d309a4fa0433d6d50b
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-02-21 08:25:19 +0100

    add normal terminal

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,6 @@
 local keymap_opts = {
-  noremap = true,
-  silent = true,
+	noremap = true,
+	silent = true,
 }
 
 return
@@ -23,5 +23,33 @@ return
 	config = function()
 		local snacks = require("snacks")
 		vim.keymap.set("n", "<C-g>", function() snacks.lazygit() end, keymap_opts)
+		-- create a command to open terminal in a float
+		vim.api.nvim_create_user_command(
+			"Mycli",
+			function()
+				snacks.terminal.toggle(
+					nil,
+					{
+						auto_close = false,
+						win = { position = "float" },
+					}
+				)
+			end,
+			{}
+		)
+		vim.keymap.set(
+			{ "n", "t" },
+			"<F2>",
+			function()
+				snacks.terminal.toggle(
+					nil,
+					{
+						auto_close = false,
+						win = { position = "float" },
+					}
+				)
+			end,
+			keymap_opts
+		)
 	end
 }
