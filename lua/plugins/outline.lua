5f5b167a22b956daad69146ad8f229155064e73c
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-02-21 14:43:55 +0100

    reconfigure outline

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -6,9 +6,19 @@ return {
 		vim.keymap.set(
 			"n",
 			"<C-h>",
-			"<cmd>Outline<CR>",
+			"<cmd>OutlineFocus<CR>",
 			keymap_opts
 		)
+
+		local outline = require("outline")
+
+		-- open on vim start
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				outline.open()
+				outline.focus_code()
+			end
+		})
 
 		require("outline").setup {
 			outline_window = {
@@ -16,6 +26,7 @@ return {
 				auto_close = true,
 				focus_on_open = true,
 				width = 15,
+				position = 'left',
 			},
 			outline_items = {
 				show_symbol_details = false,
@@ -24,5 +35,28 @@ return {
 				goto_location = '<C-j>',
 			},
 		}
+
+		vim.api.nvim_create_autocmd("WinClosed", {
+			callback = function()
+				vim.schedule(function()
+					local wins = vim.api.nvim_list_wins()
+					local non_floating_wins = {}
+					for _, win in ipairs(wins) do
+						local config = vim.api.nvim_win_get_config(win)
+						-- Only consider non-floating windows.
+						if config.relative == "" then
+							table.insert(non_floating_wins, win)
+						end
+					end
+					if #non_floating_wins == 1 then
+						local win = non_floating_wins[1]
+						local buf = vim.api.nvim_win_get_buf(win)
+						if vim.bo[buf].filetype == "Outline" then
+							vim.cmd("q")
+						end
+					end
+				end)
+			end,
+		})
 	end,
 }
