ad19b893240e0eac386c2f86fb223dbf540d8ebe
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-02-25 14:58:03 +0100

    update maps in mini files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,12 +2,29 @@ return {
 	'echasnovski/mini.nvim',
 	version = '*',
 	config = function()
-		require('mini.files').setup()
-		-- configure a keymap
-		-- MiniFiles.open(vim.api.nvim_buf_get_name(0))
+		local config = {
+			mappings = {
+				close       = 'q',
+				go_in       = 'l',
+				go_in_plus  = '<c-j>',
+				go_out      = 'h',
+				go_out_plus = 'H',
+				mark_goto   = "'",
+				mark_set    = 'm',
+				reset       = '<BS>',
+				reveal_cwd  = '@',
+				show_help   = 'g?',
+				synchronize = '=',
+				trim_left   = '<',
+				trim_right  = '>',
+			}
+		}
+
+		require('mini.files').setup(config)
+
 		vim.keymap.set('n', '<c-n>', function()
 			MiniFiles.open(vim.api.nvim_buf_get_name(0))
-      MiniFiles.reveal_cwd()
+			MiniFiles.reveal_cwd()
 		end)
 	end
 }
