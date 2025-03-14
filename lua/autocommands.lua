c64d69abf58e0ee38204af19e56ce1f9c78a4906
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-03-14 13:28:01 +0100

    feat: Add support for sourcing local .nvim.lua configuration files

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -35,3 +35,15 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		end
 	end,
 })
+
+-- Source local .nvim.lua configuration files
+vim.api.nvim_create_autocmd({ "VimEnter" },
+	{
+		callback = function()
+			local local_lua_file = vim.fn.getcwd() .. "/.nvim.lua"
+			if vim.fn.filereadable(local_lua_file) == 1 then
+				vim.cmd("source " .. local_lua_file)
+			end
+		end,
+		desc = "Source local .nvim.lua configuration files"
+	})
