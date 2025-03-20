4ad97ca6f5d17ac70aa6de0ddfeae41b3342dfe4
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-03-20 07:47:49 +0100

    refactor: Swap terminal keymaps and add checktime after toggle

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -23,26 +23,29 @@ return
 	config = function()
 		local snacks = require("snacks")
 		vim.keymap.set("n", "<C-g>", function() snacks.lazygit() end, keymap_opts)
-		-- create a command to open terminal in a float
-		vim.api.nvim_create_user_command(
-			"Mycli",
-			function()
-				snacks.terminal.toggle(
-					nil,
-					{
-						auto_close = false,
-						win = { position = "float" },
-					}
-				)
-			end,
-			{}
-		)
+
 		vim.keymap.set(
 			{ "n", "t" },
 			"<F1>",
 			function()
 				snacks.terminal.toggle(
 					"claude",
+					{
+						auto_close = false,
+						win = { position = "float" },
+					}
+				)
+				vim.cmd("checktime")
+			end,
+			keymap_opts
+		)
+
+		vim.keymap.set(
+			{ "n", "t" },
+			"<F2>",
+			function()
+				snacks.terminal.toggle(
+					nil,
 					{
 						auto_close = false,
 						win = { position = "float" },
