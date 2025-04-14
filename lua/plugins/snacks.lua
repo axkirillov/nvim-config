6399b808056552b3a16f3a0514c75d9da6be94cf
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-04-14 15:03:12 +0200

    feat(snacks): add send text to terminal function
    Introduce send_to_terminal function to send commands to open
    terminals. Add RunTestInTerminal command to execute tests in a terminal.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,6 +8,27 @@ local term_opts = {
 	win = { position = "float" },
 }
 
+local function send_to_terminal(text)
+	local term = require("snacks.terminal").get(nil, term_opts)
+
+	if not term then
+		vim.notify("Please open a terminal first.", vim.log.levels.INFO)
+		return
+	end
+
+	if term and term:buf_valid() then
+		local chan = vim.api.nvim_buf_get_var(term.buf, "terminal_job_id")
+		if chan then
+			text = text:gsub("\n", " ")
+			vim.api.nvim_chan_send(chan, text)
+		else
+			vim.notify("No terminal job found!", vim.log.levels.ERROR)
+		end
+	else
+		vim.notify("Please open a terminal first.", vim.log.levels.INFO)
+	end
+end
+
 ---@param win snacks.win
 local function close_other_terminals(win)
 	local snacks = require("snacks")
@@ -43,11 +64,24 @@ vim.keymap.set(
 	"<F1>",
 	function()
 		local snacks = require("snacks")
-		local win = snacks.terminal.toggle("claude", term_opts)
+		local win = snacks.terminal.toggle(nil, term_opts)
 		close_other_terminals(win)
 		vim.cmd("checktime")
 	end,
 	keymap_opts
+)
+
+vim.api.nvim_create_user_command(
+	"RunTestInTerminal",
+	function()
+		local snacks = require("snacks")
+		local filename = vim.fn.expand('%:t:r')
+
+		snacks.terminal.toggle(nil, term_opts)
+
+		send_to_terminal(string.format("./test.sh %s", filename))
+	end,
+	{}
 )
 
 vim.keymap.set(
@@ -55,7 +89,7 @@ vim.keymap.set(
 	"<F2>",
 	function()
 		local snacks = require("snacks")
-		local win = snacks.terminal.toggle(nil, term_opts)
+		local win = snacks.terminal.toggle("claude", term_opts)
 		close_other_terminals(win)
 		vim.cmd("checktime")
 	end,
