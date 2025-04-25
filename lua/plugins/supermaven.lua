83bf8d4b114ba8d5254aac3801e37fc9f4e33d62
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-04-25 09:20:39 +0200

    refactor(supermaven): use plugin keymaps

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -2,28 +2,11 @@ return {
 	"supermaven-inc/supermaven-nvim",
 	config = function()
 		local supermaven = require("supermaven-nvim")
-		local completion_preview = require("supermaven-nvim.completion_preview")
 		supermaven.setup({
-			disable_keymaps = true,
+			keymaps = {
+				accept_suggestion = "<Tab>",
+				accept_word = "<M-]>",
+			},
 		})
-		vim.keymap.set(
-			'i',
-			'<m-f>',
-			function()
-				if completion_preview.has_suggestion() then
-					completion_preview.on_accept_suggestion_word()
-					return
-				end
-
-				require("readline").forward_word()
-			end,
-			{ silent = true }
-		)
-		vim.keymap.set(
-			'i',
-			'<Tab>',
-			completion_preview.on_accept_suggestion,
-			{ silent = true }
-		)
 	end,
 }
