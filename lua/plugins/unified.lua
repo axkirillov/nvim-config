6d88db80f676fbf66ec639e5d3fad8ff315110e2
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2025-06-09 11:08:04 +0200

    chore(unified.nvim): setup config and add keymaps

--- lua/plugins/unified.lua
+++ lua/plugins/unified.lua
@@ -1,7 +1,7 @@
+vim.keymap.set('n', ']h', function() require('unified.navigation').next_hunk() end)
+vim.keymap.set('n', '[h', function() require('unified.navigation').previous_hunk() end)
+
 return {
 	dir = '~/repo/unified.nvim',
-	config = function()
-		require('unified').setup({
-		})
-	end
+	opts = {},
 }
